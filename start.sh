ae20d685d49fbd17ad8a0991401ac8d2077f966a
kokadekk-create <kokadekk@gmail.com>
2026-03-06 01:33:31 +0530

    change backend port

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start FastAPI backend internally
-uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
+uvicorn backend.main:app --host 0.0.0.0 --port 9000 &
 
 # Start Streamlit as the public app
 streamlit run frontend/app.py \
